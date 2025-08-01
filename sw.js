4ca983e456102a72b0df29815acd09563568604b
ShounakPalnitkar <ShounakPalnitkar1999@gmail.com>
2025-08-02 05:13:25 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,22 @@
-const CACHE_NAME = 'ckd-tool-v1';
+const CACHE_NAME = 'ckd-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/styles/main.css',
-  '/scripts/main.js',
-  '/images/logo.png'
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
